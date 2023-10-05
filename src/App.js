5cc4ef5bc682458787a46bb77c4fee85ee9b10d6
Ngash01 <nganga.amos001@gmail.com>
2023-10-05 14:15:17 +0300

    updated home page path

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
       <Router>
       <TopBar/>
         <Routes>
-          <Route path='/' element={user ? <Home/> : <Register/>}/>
+          <Route path='/' element={user ? <FollowSuggestion/> : <Register/>}/>
           <Route path='/profile/:username' element={!user ? <Navigate to={"/register"}/> : <Profile/>}/>
           <Route path='/login' element={user ? <Navigate to={'/follow'}/> :  <Login/>}/>
           <Route path='/follow' element={user ? <FollowSuggestion/> : <Register/>}/>
